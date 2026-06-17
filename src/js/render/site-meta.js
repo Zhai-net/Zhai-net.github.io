634a687b22f636f8603908f204a4b323f76e0cca
ChiaHsuanLee116 <92527749+ChiaHsuanLee116@users.noreply.github.com>
2026-06-17 15:21:18 +0800

    Delete src directory

--- src/js/render/site-meta.js
+++
@@ -1,6 +0,0 @@
-function renderSiteMeta() {
-  if (site.lastUpdated) {
-    document.querySelectorAll('[data-last-updated]')
-      .forEach((node) => { node.textContent = site.lastUpdated; });
-  }
-}
